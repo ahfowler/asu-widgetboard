d33eb147a7816848742c92abebd3b4d09e8218d7
Azaria Fowler <ahfowle1@sundevils.asu.edu>
2020-11-18 16:00:54 -0700

    Added Login.aspx page and sign in and sign out functionality.

--- Project5/Login.aspx.cs
+++ Project5/Login.aspx.cs
@@ -7,17 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Project5
 {
-    public partial class SiteMaster : MasterPage
+    public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        protected void signOut_Click(object sender, EventArgs e)
-        {
-            System.Web.Security.FormsAuthentication.SignOut();
-            Server.Transfer("Default.aspx");
         }
     }
 }
